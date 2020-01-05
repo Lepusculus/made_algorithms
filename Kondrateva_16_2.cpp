02bf2f9e61b28f59ec6e7d40c3b07e4fe23bdf27
Yulia Kondratyeva <lepus.brachylagus@gmail.com>
2020-01-05 20:44:56 +0300

    changed to function findPattern

--- Kondrateva_16_2.cpp
+++ Kondrateva_16_2.cpp
@@ -11,53 +11,21 @@
 #include <string>
 #include <vector>
 
-class ConcatView{
-private:
-    const std::string& first_;
-    const std::string& second_;
-    int len_first;
-    int len;
-public:
-    ConcatView(const std::string& first, const std::string& second): first_(first), second_(second){
-        len_first = first_.size();
-        len = len_first + second_.size();
-    }
-
-    int lenght() const{
-        return len;
-    }
-
-    int lenghtFirst() const{
-        return len_first;
-    }
-
-    char get(int i) const{
-        return i<len_first?first_[i]:second_[i-len_first];
-    }
-
-};
-
-
-int main(){
-    std::string pattern;
-    std::string str;
-    std::cin >> pattern >> str;
-    ConcatView view(pattern, str);
-
-    int k = view.lenghtFirst();
-    int n = view.lenght();
-
+std::vector<int> findPattern(const std::string& pattern, const std::string& str){
+    int k = pattern.size();
+    int n = k + str.size();
     int z = 0;
     int l = 0;
     int r = 0;
-
+    std::vector<int> ret;
+    
     std::vector<int> zcache;
     zcache.push_back(0);
     for (int i=1; i<n; ++i) {
         z = 0;
         if (i <= r)
             z = std::min (r-i+1, zcache[i-l]);
-        while (i+z < n && view.get(z) == view.get(i+z))
+        while (i+z < n && (z<k?pattern[z]:str[z-k]) == (i+z<k?pattern[i+z]:str[i + z-k]))
             ++z;
         if (i+z-1 > r) {
             l = i;
@@ -65,10 +33,22 @@ int main(){
         }
 
         if(z>=k && i>=k){
-            std::cout << i-k << ' ';
+            ret.push_back(i-k);
         }
         zcache.push_back(z);
     }
+    return ret;
+}
+
+
+int main(){
+    std::string pattern;
+    std::string str;
+    std::cin >> pattern >> str;
+    for (auto pos: findPattern(pattern, str)){
+       std::cout << pos << ' '; 
+    }
+    
     std::cout << std::endl;
 }
 
